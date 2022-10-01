414720225df9d43ca01f882adec302be356cffde
bokyung <qkrqhru@naver.com>
2022-10-01 18:17:37 +0900

    commit

--- 02/index.js
+++ 02/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Clock from './comment/Clock';
+import Clock from './timer/Clock';
 import CommentList from './comment/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
